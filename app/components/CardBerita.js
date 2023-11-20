eac3f46cb1589d26831a1da327f5d1e104249099
Silvester Kresna <silvesterkresna@gmail.com>
2023-11-21 02:05:29 +0700

    ubah

--- app/components/CardBerita.js
+++ app/components/CardBerita.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 
 export default function CardBerita({ author, title, url, publishedAt }) {
